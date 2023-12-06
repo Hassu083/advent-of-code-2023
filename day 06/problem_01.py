14d6558795788addd474174a2b6c3c99d934e582
Hassu083 <ha6582019@gmail.com>
2023-12-06 18:08:11 +0500

    Quadratic

--- day 06/problem_01.py
+++ day 06/problem_01.py
@@ -1,3 +1,4 @@
+import math
 f = open("e://advent of code//day 06//problem_01.txt")
 
 
@@ -8,15 +9,30 @@ distances  = list(map(int, lines[1][10:].strip().split('   ')))
 print(times,distances)
 totalways = 1
 
-def f(x, y):
-    ways = 0
-    for i in range(1,x+1):
-        if i*(x-i) > y:
-            ways += 1
-    return ways
+# def f(x, y):
+#     ways = 0
+#     for i in range(1,x+1):
+#         if i*(x-i) > y:
+#             ways += 1
+#     return ways
+
+# for i in range(len(times)):
+#     totalways *= f(times[i], distances[i])
+
+def quadratic(x, y):
+    common_term = math.sqrt(x*x - 4*y)
+    x0 = math.ceil((x-common_term)/2)
+    x1 = math.floor((x+common_term)/2)
+
+    if x0*(x-x0) == y:
+        x0 += 1
+    if x1*(x-x1) == y:
+        x1 -= 1
+    
+    return x1-x0+1
 
 for i in range(len(times)):
-    totalways *= f(times[i], distances[i])
+    totalways *= quadratic(times[i], distances[i])
 
 print(totalways)
 
--- day 06/problem_02.py
+++ day 06/problem_02.py
@@ -1,3 +1,4 @@
+import math
 f = open("e://advent of code//day 06//problem_02.txt")
 
 
@@ -8,12 +9,24 @@ distance  = int(''.join(map(str.strip, lines[1][10:].strip().split('  '))))
 print(time,distance)
 totalways = 1
 
+# def f(x, y):
+#     ways = 0
+#     for i in range(1,x+1):
+#         if i*(x-i) > y:
+#             ways += 1
+#     return ways
+
 def f(x, y):
-    ways = 0
-    for i in range(1,x+1):
-        if i*(x-i) > y:
-            ways += 1
-    return ways
+    common_term = math.sqrt(x*x - 4*y)
+    x0 = math.ceil((x-common_term)/2)
+    x1 = math.floor((x+common_term)/2)
+
+    if x0*(x-x0) == y:
+        x0 += 1
+    if x1*(x-x1) == y:
+        x1 -= 1
+    
+    return x1-x0+1
 
 
 print(f(time, distance))
