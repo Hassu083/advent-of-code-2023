5b2d588813795337f0cee37ffaa53654b4fceece
Hassu083 <ha6582019@gmail.com>
2023-12-11 18:58:59 +0500

    Day 11

---
+++ day 11/problem_01.py
@@ -0,0 +1,37 @@
+import math
+f = open("e://advent of code//day 11//problem_01.txt")
+
+
+lines = f.readlines()
+numberOfrows = len(lines)
+numberOfcol = len(lines[-1])
+rows = set([i for i in range(numberOfrows)])
+cols = set([i for i in range(numberOfcol)])
+ans = 0
+positions = []
+for i in range(numberOfrows):
+    for j in range(numberOfcol):
+        if lines[i][j] == "#":
+            positions.append((i,j))
+            if i in rows:
+                rows.remove(i)
+            if j in cols:
+                cols.remove(j)
+
+n = len(positions)
+for p1 in range(n):
+    for p2 in range(p1, n):
+        x1, y1 = positions[p1]
+        x2, y2 = positions[p2]
+
+        ans += abs(x2-x1) + abs(y2-y1)
+
+        for extraRow in rows:
+            if min(x1,x2) < extraRow < max(x1,x2):
+                ans += 1
+        for extraCol in cols:
+            if min(y1,y2) < extraCol < max(y1,y2):
+                ans += 1
+
+            
+print(ans)
